40ca8c8a8c6126da3a44415ff85eefc037a40555
issliu <liushun8910@gmail.com>
2018-03-29 20:51:50 +0800

    add get variable type method

--- huiwen.js
+++ huiwen.js
@@ -1,7 +1,7 @@
 // 检测一个字符串是否为回文字符串
 // 主要思路是：将字符 A(1),A(2),A(3)...A(n) 从中间拆分，比较从 A(1)-A(middle) 的每个字符串，是否与 A(n-1) 和 A(n-1-m) 分别相等
 
-function checkHuiWenString(str) {
+export function checkHuiWenString(str) {
     if (!isString(str)) return false;
     let len = str.length;
     for (let i = 0; i < parseInt(len / 2); i++) {
---
+++ type-check.js
@@ -0,0 +1,36 @@
+// ES6 中一共有如下几种数据类型：
+// 原生类型： Boolean,Number,String,Null,Undefined,Symbol
+// 引用类型：object，
+// 全局对象调用 typeof 均返回 "object", 更多情况下，我们需要具体确定到底是什么数据类型，因此先枚举可能的全局对象
+// Array, RegExp, Date, Map, WeakMap, Set, WeakSet等等，ES6 新增了非常多的全局对象，具体请参考链接：
+// https://tc39.github.io/ecma262/#sec-other-properties-of-the-global-object
+
+function getTypeOf(variable) {
+    let typeStr = Object.prototype.toString.call(variable);
+    let res = typeStr.match(/^\[object\s(.*)\]$/);
+    // console.log(res[1]);
+    return res[1];
+}
+
+// getTypeOf(true);
+// getTypeOf(1);
+// getTypeOf('abc');
+// getTypeOf(null);
+// getTypeOf(undefined);
+// getTypeOf(Symbol(1));
+// getTypeOf({});
+// getTypeOf([1,2,3]);
+// getTypeOf(new ArrayBuffer());
+// getTypeOf(new Date());
+// getTypeOf(/^abc/);
+// getTypeOf(new Map());
+// getTypeOf(new WeakMap());
+// getTypeOf(new Set());
+// getTypeOf(new WeakSet());
+// getTypeOf(new Int8Array());
+// getTypeOf(new Int16Array());
+// getTypeOf(new Error());
+// getTypeOf(new EvalError());
+// getTypeOf(new DataView());
+// getTypeOf(new Float32Array());
+// getTypeOf(new Float64Array());
